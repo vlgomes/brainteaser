3c65da8df311b385aa72cc5081e66cf0604d8c12
vlgomes <uvlgomes@gmail.com>
2017-03-15 13:49:35 +0000

    animation button

--- BrainTeaser/CustomButton.swift
+++ BrainTeaser/CustomButton.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import pop
 
 @IBDesignable
 class CustomButton: UIButton {
@@ -36,5 +37,38 @@ class CustomButton: UIButton {
     
     func setupView(){
         self.layer.cornerRadius = cornerRadius
+        
+        self.addTarget(self, action: #selector(CustomButton.scaleToSmall), for: .touchDown)
+        self.addTarget(self, action: #selector(CustomButton.scaleToSmall), for: .touchDragEnter)
+        self.addTarget(self, action: #selector(CustomButton.scaleAnimation), for: .touchUpInside)
+        self.addTarget(self, action: #selector(CustomButton.scaleDefault), for: .touchDragExit)
+    }
+    
+    func scaleToSmall(){
+        let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        
+        //animate the button to 95%
+        scaleAnim?.toValue = NSValue(cgSize: CGSize(width: 0.95, height: 0.95))
+        
+        self.layer.pop_add(scaleAnim, forKey: "layerScaleSmallAnimation")
+    }
+    
+    func scaleAnimation(){
+        let scaleAnim = POPSpringAnimation(propertyNamed: kPOPLayerScaleXY)
+        
+        scaleAnim?.velocity = NSValue(cgSize: CGSize(width: 3.0, height: 3.0))
+        
+        //when animation done, go back to normal
+        scaleAnim?.toValue = NSValue(cgSize: CGSize(width: 1.0, height: 1.0))
+        
+        scaleAnim?.springBounciness = 18
+        
+        self.layer.pop_add(scaleAnim, forKey: "layerScaleSpringAnimation")
+    }
+    
+    func scaleDefault(){
+        let scaleAnim = POPBasicAnimation(propertyNamed: kPOPLayerScaleXY)
+        scaleAnim?.toValue = NSValue(cgSize: CGSize(width: 1.0, height: 1.0))
+        self.layer.pop_add(scaleAnim, forKey: "layerScaleDefaultAnimation")
     }
 }
