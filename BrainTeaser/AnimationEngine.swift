a6ac079b970d46b97e81b6b73b3da7679ef70f00
vlgomes <uvlgomes@gmail.com>
2017-03-16 15:08:52 +0000

    animation engine

---
+++ BrainTeaser/AnimationEngine.swift
@@ -0,0 +1,63 @@
+//
+//  AnimationEngine.swift
+//  BrainTeaser
+//
+//  Created by Vasco Gomes on 15/03/2017.
+//  Copyright © 2017 Vasco Gomes. All rights reserved.
+//
+
+import UIKit
+import pop
+
+class AnimationEngine{
+    
+    class var offScreenRightPosition : CGPoint{
+        return CGPoint(x: UIScreen.main.bounds.width, y: (UIScreen.main.bounds.size.height/2))
+    }
+    
+    class var offScreenLeftPosition : CGPoint{
+        return CGPoint(x:-UIScreen.main.bounds.width, y: (UIScreen.main.bounds.size.height/2))
+    }
+    
+    class var screenCenterPosition : CGPoint{
+        return CGPoint(x:(UIScreen.main.bounds.size.width/2), y: (UIScreen.main.bounds.size.height/2))
+    }
+    
+    let ANIME_DELAY: Int = 1
+    var originalConstants = [CGFloat]()
+    var constraints: [NSLayoutConstraint]!
+    
+    init (constraints:[NSLayoutConstraint]){
+        
+        for con in constraints{
+            originalConstants.append(con.constant)
+            con.constant = AnimationEngine.offScreenRightPosition.x
+        }
+        
+        self.constraints = constraints
+    }
+    
+    func animateOnScreen(){
+        
+        var index = 0
+        repeat{
+                
+            let moveAnim = POPSpringAnimation(propertyNamed: kPOPLayoutConstraintConstant)
+                
+            moveAnim?.toValue = self.originalConstants[index]
+                
+            moveAnim?.springBounciness = 12
+            moveAnim?.springSpeed = 12
+            
+            if (index > 0){
+                moveAnim?.dynamicsFriction += 10 + CGFloat(index)
+            }
+                
+            let con = self.constraints[index]
+            con.pop_add(moveAnim, forKey: "moveOnScreen")
+                
+            index += 1
+                
+        } while (index<self.constraints.count)
+    }
+}
--- BrainTeaser/LoginVC.swift
+++ BrainTeaser/LoginVC.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet var emailConstraint: NSLayoutConstraint!
+    @IBOutlet var passwordConstraint: NSLayoutConstraint!
+    @IBOutlet var loginConstraint: NSLayoutConstraint!
+    
+    var animEngine : AnimationEngine!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.animEngine = AnimationEngine(constraints: [emailConstraint,passwordConstraint,loginConstraint])
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
+    override func viewDidAppear(_ animated: Bool) {
+        self.animEngine.animateOnScreen()
+    }
 }
 
