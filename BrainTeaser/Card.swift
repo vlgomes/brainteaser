2c62a3d9ee0dfdf4cfc804f1b54f979612506456
vlgomes <uvlgomes@gmail.com>
2017-03-16 16:33:46 +0000

    custom-card

---
+++ BrainTeaser/Card.swift
@@ -0,0 +1,43 @@
+//
+//  Card.swift
+//  BrainTeaser
+//
+//  Created by Vasco Gomes on 16/03/2017.
+//  Copyright © 2017 Vasco Gomes. All rights reserved.
+//
+
+import UIKit
+
+class Card: UIView {
+
+    let shapes = ["shape1","shape2","shape3"]
+    
+    var currentShape: String!
+    
+    @IBOutlet weak var shapeImage: UIImageView!
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet{
+            setupView()
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    func setupView(){
+        
+        self.layer.shadowOpacity = 0.8
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowColor = UIColor(red: 157.0/255.0, green: 157.0/255.0, blue: 157.0/255.0, alpha: 1.0).cgColor
+        self.setNeedsLayout()
+    }
+    
+    func selectShape(){
+        currentShape = shapes[Int(arc4random_uniform(3))]
+        shapeImage.image = UIImage(named: currentShape)
+    }
+}
---
+++ BrainTeaser/GameVC.swift
@@ -0,0 +1,21 @@
+//
+//  GameVC.swift
+//  BrainTeaser
+//
+//  Created by Vasco Gomes on 16/03/2017.
+//  Copyright © 2017 Vasco Gomes. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
